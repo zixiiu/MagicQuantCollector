c12a46854dab2eeccf7195d08df9b1a2d8708911
zixiiu <zixiufu@gmail.com>
2022-05-22 22:42:45 +0800

    add notification by PUSHDEER.

--- MainLoop.py
+++ MainLoop.py
@@ -3,6 +3,7 @@ import time
 import datetime
 import NewData
 import variables
+import Notification
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s:%(message)s')
@@ -14,11 +15,13 @@ while True:
     try:
         if datetime.datetime.now().strftime('%H') == '00' or gostright:
             NewData.update_data()
+            Notification.send_notification("MQCollector SUCCESS.")
         else:
             logging.debug('not the time yet.')
         time.sleep(3600)
     except Exception as e:
         logging.exception("message")
+        Notification.send_notification("MQCollector ERROR, check log.")
         time.sleep(3600)
     except KeyboardInterrupt:
         raise KeyboardInterrupt
--- Notification.py
+++ Notification.py
@@ -1,1 +1,9 @@
-# TODO: some notification?
+import requests
+import variables
+
+def send_notification(content):
+    url = 'https://api2.pushdeer.com/message/push?pushkey=%s&text=%s' %(variables.push_key, content)
+    requests.get(url)
+
+if __name__ == '__main__':
+    send_notification('test_notification')
