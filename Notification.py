75fea3ed4be9ec2cba47ede5a62fe8c20cd60b58
zixiiu <zixiufu@gmail.com>
2022-05-26 20:01:04 +0800

    notification error passthrough

--- Notification.py
+++ Notification.py
@@ -2,8 +2,11 @@ import requests
 import variables
 
 def send_notification(content):
-    url = 'https://api2.pushdeer.com/message/push?pushkey=%s&text=%s' %(variables.push_key, content)
-    requests.get(url)
+    try:
+        url = 'https://api2.pushdeer.com/message/push?pushkey=%s&text=%s' %(variables.push_key, content)
+        requests.get(url)
+    except Exception:
+        pass
 
 if __name__ == '__main__':
     send_notification('test_notification')
