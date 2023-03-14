ebb38df96671ec31868e0a8ed4c861f0a9a79c37
hupa <zixiufu@gmail.com>
2023-03-14 12:39:04 +0800

    update for GKW server.

--- SQLEngine.py
+++ SQLEngine.py
@@ -7,7 +7,7 @@ class engine():
         # test
         # db_path = "mysql+pymysql://%s:%s@%s/TestQuantDB?charset=utf8mb4"
         # production
-        db_path = "mysql+pymysql://%s:%s@%s/MainQuantDB?charset=utf8mb4"
+        db_path = "mysql+pymysql://%s:%s@%s/quantdb_prod?charset=utf8mb4"
         self.engine = create_engine \
             (db_path %
              (variables.db_username, variables.db_pw, variables.db_address))
